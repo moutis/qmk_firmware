21411b355e725c9637eed85398db9cd885676375
moutis <35399827+moutis@users.noreply.github.com>
2025-09-05 16:32:45 -0400

    remove unused features on by default

--- keyboards/checkerboards/candybar_ortho/keymaps/moutis/rules.mk
+++ keyboards/checkerboards/candybar_ortho/keymaps/moutis/rules.mk
@@ -1,6 +1,14 @@
 # Candybar Ortho hardware features
 ENCODER_ENABLE = no        # Enables the use of one or more encoders
 
-#RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow (disabled on AVR to save space)
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow (disabled on AVR to save space)
 OLED_ENABLE = no           # Enables the use of OLED displays
-OLED_DRIVER = SSD1306
+#OLED_DRIVER = SSD1306
+
+MOUSEKEY_ENABLE = no
+BOOTMAGIC_ENABLE = no
+CONSOLE_ENABLE = no
+EXTRAKEY_ENABLE = no
+COMMAND_ENABLE = no
+AUDIO_ENABLE = no
+LEADER_ENABLE = no
