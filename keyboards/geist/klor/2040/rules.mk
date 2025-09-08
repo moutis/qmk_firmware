59033dc946165bebc304e608e62dcab6fad8c406
moutis <35399827+moutis@users.noreply.github.com>
2025-09-08 09:41:47 -0400

    update klor for RP2040

--- keyboards/geist/klor/2040/rules.mk
+++ keyboards/geist/klor/2040/rules.mk
@@ -11,15 +11,15 @@ ALLOW_WARNINGS = yes
 LTO_ENABLE = no
 
 # PIO serial/WS2812 drivers must be used on RP2040
-SERIAL_DRIVER = vendor
-WS2812_DRIVER = vendor
+#SERIAL_DRIVER = vendor
+#WS2812_DRIVER = vendor
 
 # CONVERTER - if you use a listed MCU comment the first line and uncomment the appropiate line
-CONVERT_TO = promicro_rp2040
+#CONVERT_TO = promicro_rp2040
 #CONVERT_TO = kb2040
 #CONVERT_TO = blok
 #CONVERT_TO = elite_pi
 
 # AUDIO 
-AUDIO_ENABLE = yes
-AUDIO_DRIVER = pwm_hardware
+#AUDIO_ENABLE = yes
+#AUDIO_DRIVER = pwm_hardware
---
+++ keyboards/geist/klor/keymaps/moutis/keymap.c
@@ -0,0 +1,9 @@
+//#include QMK_KEYBOARD_H
+
+#include <users/moutis/moutis.h>
+
+/* include the config.qmk.fm json compiled to c using
+* "QMK json2c -o users/moutis/ferris/ferris_hd_keymap.c users/moutis/ferris/ferris_hd_neu_sk-trim.json"
+*/
+
+#include <users/moutis/mykeyboards/klor/keymap.c>
