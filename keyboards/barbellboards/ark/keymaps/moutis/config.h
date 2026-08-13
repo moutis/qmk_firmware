3f8f88af3e34ac02faff37c136ea1be5633c4f04
moutis <35399827+moutis@users.noreply.github.com>
2026-08-13 14:00:02 -0400

    Add Arkenswoop

---
+++ keyboards/barbellboards/ark/keymaps/moutis/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define KEYBOARD_rollow_rev1
+
+//#include "rev1.h"
+
+#include <users/moutis/boards/rollow/config.h>
---
+++ keyboards/barbellboards/ark/keymaps/moutis/rules.mk
@@ -0,0 +1,7 @@
+# Rollow hardware features
+BOOTMAGIC_ENABLE = no     # Enable Bootmagic Lite
+ENCODER_ENABLE = yes        # Enables the use of one or more encoders
+
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
+OLED_ENABLE = no           # Enables the use of OLED displays
+OLED_DRIVER = SSD1306
